2a34fa61724201427b643970a739d50358e28d43
yaojia <2317321437@qq.com>
2019-03-10 22:23:41 +0800

    7.4.1抽象类与抽象方法——模拟"去商场买衣服"

--- src/main/java/com/soft1841/chapter7/GoShopping.java
+++ src/main/java/com/soft1841/chapter7/GoShopping.java
@@ -3,6 +3,8 @@ package com.soft1841.chapter7;
 /**
  * 子类，继承自Market抽象类，实现其中的shop()抽象方法
  * 具体什么商场,买什么东西
+ * @author sijia
+ * 2019.3.10
  */
 public class GoShopping {
     public static void main(String[] args) {
