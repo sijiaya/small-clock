ef6aa4240e92d22f802afb8aa2bfd35360e5f70d
sijiaya <2317321437@qq.com>
2019-04-08 06:11:10 +0800

    实现线程的两种方式：继承Thread类和实现Runnable接口，输出相同结果

---
+++ src/main/java/com/soft1841/chapter13/RunnableTest.java
@@ -0,0 +1,24 @@
+package com.soft1841.chapter13;
+
+
+//用Object类的子类实现Runnable接口
+public class RunnableTest extends Object implements Runnable{
+    private int count = 10;
+    public void run() {
+        while (true){
+            //打印count变量
+            System.out.println(count+"  ");
+            if (--count==0){
+                //使count变量自减，当自减为0时，退出循环
+                break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        //创建线程类的一个对象
+        ThreadTest test = new ThreadTest();
+        //启动线程
+        test.start();
+    }
+}
