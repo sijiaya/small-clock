de0084841bc60a4aea8b6603392d854341541a02
sijiaya <2317321437@qq.com>
2019-04-12 06:16:06 +0800

    线程的恢复与暂停

--- src/main/java/com/soft1841/chapter15/DrawImageTest.java
+++ src/main/java/com/soft1841/chapter15/DrawImageTest.java
@@ -30,7 +30,7 @@ public class DrawImageTest extends JFrame {
             Graphics2D g2 = (Graphics2D) g;
             //获取图片资源
 //            Image img = new ImageIcon("/img/1.jpg").getImage();
-            Image img = new ImageIcon(this.getClass().getResource("img/1.jpg")).getImage();
+            Image img = new ImageIcon(this.getClass().getResource("resources/img/1.jpg")).getImage();
             //显示图像
             g2.drawImage(img,0,0,this);
         }
